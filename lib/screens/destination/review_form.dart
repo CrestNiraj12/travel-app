f05aa2ab2f2b433fae5b1e0c89769ed0c364478c
CrestNiraj12 <crestniraj@gmail.com>
2023-05-25 10:21:15 +0545

    feat: updates

--- lib/screens/destination/review_form.dart
+++ lib/screens/destination/review_form.dart
@@ -84,139 +84,141 @@ class _ReviewFormState extends ConsumerState<ReviewForm> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
-      child: SizedBox(
-        height: 450,
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 24.0,
-                vertical: 50.0,
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Provide your ratings: ",
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            fontSize: 16,
-                          ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Center(
-                      child: RatingBar.builder(
-                        initialRating: 0,
-                        minRating: 0,
-                        maxRating: 5,
-                        direction: Axis.horizontal,
-                        itemSize: 40,
-                        allowHalfRating: true,
-                        itemCount: 5,
-                        itemPadding: EdgeInsets.symmetric(
-                          horizontal: 0.0,
-                        ),
-                        itemBuilder: (context, _) => Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        ),
-                        onRatingUpdate: (rating) {
-                          ref.read(_ratingProvider.notifier).state = rating;
-                        },
+      child: SingleChildScrollView(
+        child: SizedBox(
+          height: 450,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                  vertical: 50.0,
+                ),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Provide your ratings: ",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              fontSize: 16,
+                            ),
                       ),
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    Text(
-                      "Write your review (Optional): ",
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            fontSize: 16,
-                          ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      controller: _controller,
-                      style: TextStyle(
-                        fontSize: 13,
-                        height: 1.3,
+                      SizedBox(
+                        height: 10,
                       ),
-                      decoration: InputDecoration(
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 1,
-                            color: Colors.grey,
+                      Center(
+                        child: RatingBar.builder(
+                          initialRating: 0,
+                          minRating: 0,
+                          maxRating: 5,
+                          direction: Axis.horizontal,
+                          itemSize: 40,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(
+                            horizontal: 0.0,
                           ),
-                        ),
-                        focusedBorder: const OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.grey,
-                            width: 0.0,
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
                           ),
+                          onRatingUpdate: (rating) {
+                            ref.read(_ratingProvider.notifier).state = rating;
+                          },
                         ),
                       ),
-                      maxLines: 6,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        isAuthenticated
-                            ? ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: isLoading
-                                      ? Colors.grey
-                                      : Colors.blueAccent,
+                      SizedBox(
+                        height: 40,
+                      ),
+                      Text(
+                        "Write your review (Optional): ",
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              fontSize: 16,
+                            ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        controller: _controller,
+                        style: TextStyle(
+                          fontSize: 13,
+                          height: 1.3,
+                        ),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          focusedBorder: const OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.grey,
+                              width: 0.0,
+                            ),
+                          ),
+                        ),
+                        maxLines: 6,
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          isAuthenticated
+                              ? ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: isLoading
+                                        ? Colors.grey
+                                        : Colors.blueAccent,
+                                  ),
+                                  onPressed: isLoading
+                                      ? null
+                                      : () => _handleSubmit(ratings),
+                                  child: isLoading
+                                      ? SizedBox(
+                                          height: 30,
+                                          width: 30,
+                                          child: Padding(
+                                            padding: EdgeInsets.symmetric(
+                                              vertical: 7,
+                                              horizontal: 7,
+                                            ),
+                                            child: CircularProgressIndicator(
+                                              strokeWidth: 2,
+                                              color: Colors.blueAccent,
+                                            ),
+                                          ),
+                                        )
+                                      : Text('Submit'),
+                                )
+                              : Text(
+                                  'You need to login first to submit review 😊',
+                                  style: Theme.of(context).textTheme.bodyLarge,
                                 ),
-                                onPressed: isLoading
-                                    ? null
-                                    : () => _handleSubmit(ratings),
-                                child: isLoading
-                                    ? SizedBox(
-                                        height: 30,
-                                        width: 30,
-                                        child: Padding(
-                                          padding: EdgeInsets.symmetric(
-                                            vertical: 7,
-                                            horizontal: 7,
-                                          ),
-                                          child: CircularProgressIndicator(
-                                            strokeWidth: 2,
-                                            color: Colors.blueAccent,
-                                          ),
-                                        ),
-                                      )
-                                    : Text('Submit'),
-                              )
-                            : Text(
-                                'You need to login first to submit review 😊',
-                                style: Theme.of(context).textTheme.bodyLarge,
-                              ),
-                      ],
-                    ),
-                  ],
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Positioned(
-              top: 20,
-              right: 20,
-              child: IconButton(
-                icon: Icon(Icons.close),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
+              Positioned(
+                top: 20,
+                right: 20,
+                child: IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
